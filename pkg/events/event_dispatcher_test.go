84796db76b1ce83c78d3d6b97f7a1c09e70d48f6
Kayky Freitas <kayky_defreitas@hotmail.com>
2024-06-15 19:25:14 -0300

    feat(event_dispatcher): add event dispatcher register

---
+++ pkg/events/event_dispatcher_test.go
@@ -0,0 +1,79 @@
+package events
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event1          TestEvent
+	event2          TestEvent
+	handler1        TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (suite *EventDispatcherTestSuite) SetupTest() {
+	suite.eventDispatcher = NewEventDispatcher()
+	suite.handler1 = TestEventHandler{ID: 1}
+	suite.handler2 = TestEventHandler{ID: 2}
+	suite.handler3 = TestEventHandler{ID: 3}
+	suite.event1 = TestEvent{Name: "teste1", Payload: "Teste 1"}
+	suite.event2 = TestEvent{Name: "teste2", Payload: "Teste 2"}
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	assert.Equal(suite.T(), &suite.handler1, suite.eventDispatcher.handlers[suite.event1.GetName()][0])
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event1.GetName()][1])
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
+	err := suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event1.GetName(), &suite.handler1)
+	suite.Equal(ErrHandlerAlreadyRegistered, err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event1.GetName()]))
+
+	assert.Equal(suite.T(), &suite.handler1, suite.eventDispatcher.handlers[suite.event1.GetName()][0])
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}
